daa154cdd46755e04d2c9d6e005870713e106c38
wang-jiahua <90542846+wang-jiahua@users.noreply.github.com>
2025-04-08 08:29:29 +0000

    Deploying to gh-pages from @ wang-jiahua/arceos@d1f0c64459999f117b0ee35dc2aca3e70beb76c6 🚀

---
+++ trait.impl/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["arceos_posix_api",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"arceos_posix_api/struct.FilePath.html\" title=\"struct arceos_posix_api::FilePath\">FilePath</a>"]]],["axdma",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"axdma/struct.BusAddr.html\" title=\"struct axdma::BusAddr\">BusAddr</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[305,270]}
---
+++ trait.impl/core/ops/deref/trait.DerefMut.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["axhal",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"axhal/arch/struct.UspaceContext.html\" title=\"struct axhal::arch::UspaceContext\">UspaceContext</a>"]]],["axstd",[["impl&lt;T: ?<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"axstd/sync/struct.MutexGuard.html\" title=\"struct axstd::sync::MutexGuard\">MutexGuard</a>&lt;'_, T&gt;"]]],["axsync",[["impl&lt;T: ?<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"axsync/struct.MutexGuard.html\" title=\"struct axsync::MutexGuard\">MutexGuard</a>&lt;'_, T&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[305,458,450]}
